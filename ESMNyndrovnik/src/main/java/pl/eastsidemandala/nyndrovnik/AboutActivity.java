7a0a19a1a936149567e725c82426b9b8bd233cd4
Jan Waga <janek.0x77@gmail.com>
2024-10-31 22:05:47 +0100

    support lib -> androidx migration by IDE

--- ESMNyndrovnik/src/main/java/pl/eastsidemandala/nyndrovnik/AboutActivity.java
+++ ESMNyndrovnik/src/main/java/pl/eastsidemandala/nyndrovnik/AboutActivity.java
@@ -3,7 +3,7 @@ package pl.eastsidemandala.nyndrovnik;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import androidx.fragment.app.FragmentActivity;
 import android.text.Html;
 import android.view.View;
 import android.widget.TextView;
--- ESMNyndrovnik/src/main/java/pl/eastsidemandala/nyndrovnik/NyndroFragment.java
+++ ESMNyndrovnik/src/main/java/pl/eastsidemandala/nyndrovnik/NyndroFragment.java
@@ -8,7 +8,7 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
